e7c85fabb453268fe214d277c51318edf19247aa
= <=>
2024-07-25 15:48:14 -0400

    docker issues plague me

--- backend/src/main/java/com/jakemaxell/Open/Source/IT/Ticketing/System/OpenSourceItTicketingSystemApplication.java
+++ backend/src/main/java/com/jakemaxell/Open/Source/IT/Ticketing/System/OpenSourceItTicketingSystemApplication.java
@@ -3,9 +3,10 @@ package com.jakemaxell.Open.Source.IT.Ticketing.System;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class OpenSourceItTicketingSystemApplication {
+public class OpenSourceItTicketingSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenSourceItTicketingSystemApplication.class, args);
