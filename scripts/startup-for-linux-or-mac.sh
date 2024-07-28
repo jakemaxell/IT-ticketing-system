ecba3932729fb67c7e1711d34c98f35bf72887d0
Jacob Maxwell <37555603+jakemaxell@users.noreply.github.com>
2024-07-28 14:07:16 -0400

    Scripts (#11)
    * test
    
    * Updated checks name
    
    * Testing the github CI/CD deployment
    
    * Updated the scripts to account for new directory organization

--- scripts/startup-for-linux-or-mac.sh
+++ scripts/startup-for-linux-or-mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./backend || { echo "Failed to change directory to ./backend"; exit 1; }
+cd ../backend || { echo "Failed to change directory to ./backend"; exit 1; }
 
 mvn clean install -DskipTests || { echo "Maven build failed"; exit 1; }
 
