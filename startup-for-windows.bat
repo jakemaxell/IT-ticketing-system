018dd5f32d4016882aa2a3943e69c025def8623c
jakemaxell <jakemaxwell97@gmail.com>
2024-07-27 23:00:50 -0400

    Updated all the Dockerfiles and the docker-compose.yaml \ fixed the issue with tomcat running the webapp \ added the Docker configurations for running the react front end application and tested the networking \ added a simple startup script for windows deployment to docker

---
+++ startup-for-windows.bat
@@ -0,0 +1,21 @@
+@echo off
+
+cd ./backend
+
+call mvn clean install -DskipTests || goto :error
+
+cd ..
+
+docker compose up -d || goto :error
+
+cd ./backend
+
+mvn clean || goto :error
+
+goto :end
+
+:error
+echo An error occurred during the execution of the script.
+pause
+
+:end
