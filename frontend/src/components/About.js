760d1d43305bb35362c5fe712ddb42147ff36666
jakemaxell <jakemaxwell97@gmail.com>
2024-07-28 13:31:32 -0400

    Added base template for about page and updated the github workflow

---
+++ frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import NavBar from "./Navbar";
+
+function About(){
+
+    return (
+        <div>
+            <NavBar/>
+            <h1>About Page</h1>
+        </div>
+    );
+};
+
+export default About;
