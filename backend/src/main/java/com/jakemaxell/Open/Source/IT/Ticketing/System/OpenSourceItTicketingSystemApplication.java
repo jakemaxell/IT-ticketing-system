c46f370a7da0853a71056746021324e8973c41b6
= <jakemaxwell97@gmail.com>
2024-08-05 10:40:14 -0400

    Cleaning up and fixing node.yml issues locally

--- backend/src/main/java/com/jakemaxell/Open/Source/IT/Ticketing/System/OpenSourceItTicketingSystemApplication.java
+++ backend/src/main/java/com/jakemaxell/Open/Source/IT/Ticketing/System/OpenSourceItTicketingSystemApplication.java
@@ -3,7 +3,6 @@ package com.jakemaxell.Open.Source.IT.Ticketing.System;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
